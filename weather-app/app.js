925f96bd4f5ad8f00a8341faba3594f61f0f3e42
HyeManMoon <hyemanmoon@gmail.com>
2018-02-07 12:24:30 -0800

    added weather api request

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,30 +1,38 @@
+// const yargs = require('yargs');
+
+// const geocode = require('./geocode/geocode');
+
+// const argv = yargs.options({
+//     a: {
+//         demand: true,
+//         alias: 'address',
+//         describe: "Address to fetch weather for",
+//         string: true
+//     }
+// }).help().alias('help','h').argv;
+
+// geocode.geocodeAddress(argv.address, (error, results) => {
+//     if(error) {
+//         console.log(error);
+//     } else {
+//         console.log(JSON.stringify(results, undefined, 2));
+//     }
+// });
+
+
+//2fea7132af7e13a7f8367ad180a6e2a6
+
 const request = require('request');
-const yargs = require('yargs');
-
-const argv = yargs.options({
-    a: {
-        demand: true,
-        alias: 'address',
-        describe: "Address to fetch weather for",
-        string: true
-    }
-}).help().alias('help','h').argv;
-
-var encodedAddress = encodeURIComponent(argv.address);
-
 
 request({
-    url: `http://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
+    url: `https://api.darksky.net/forecast/2fea7132af7e13a7f8367ad180a6e2a6/33.8276052,-117.9073244`,
     json: true
 }, (error, response, body) => {
     if(error) {
-        console.log("unable to connect to the servers")
-    } else if(body.status === 'ZERO_RESULTS'){
-        console.log("unable to find that address");
-    } else if(body.status === 'OK') {
-        console.log(`Address: ${body.results[0].formatted_address}`);
-        console.log(`Long: ${body.results[0].geometry.location.lng}`);
-        console.log(`Lat: ${body.results[0].geometry.location.lat}`);
+        console.log("unable to connect to the server")
+    } else if(body.status === 'The given location is invalid') {
+        console.log("unable to find that location");
+    } else {
+        console.log( body.currently.temperature);
     }
-
-});
+})
